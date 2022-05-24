9fbc1b84e8aa695ab427901fb29c8cfaa438bc03
Dark15 <starrycat15@gmail.com>
2022-05-24 17:40:21 +0800

    chore: Add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier'
+  ]
+}
