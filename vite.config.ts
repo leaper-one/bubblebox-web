73be85861a8e725f4c734943d63ba0671b0eaf55
Dark15 <starrycat15@gmail.com>
2022-05-27 17:10:38 +0800

    chore: Support auto import components

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()]
+  plugins: [
+    vue(),
+    WindiCSS(),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: false
+    })
+  ]
 })
