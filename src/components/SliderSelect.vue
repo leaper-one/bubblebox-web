7b54205eaa6fcf4ef523820ee5c81d21412f2081
JulienZeng <77975074+JulienZeng@users.noreply.github.com>
2022-05-29 06:21:43 +0800

    粗略实现滑动选择的组件

---
+++ src/components/SliderSelect.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  title: string
+  marks: Record<string, string>
+  defaultValue: number
+  step?: number
+  max: number
+  min: number
+  usePercent?: boolean
+}>()
+
+const value = ref(props.defaultValue)
+const step = ref(props.step).value ? 1 : 'mark'
+</script>
+
+<template>
+  <n-list bordered>
+    <n-list-item>
+      <template #default>
+        <div style="display: flex; flex: auto; flex-direction: row">
+          <div class="prefix">{{ props.title }}</div>
+          <div class="default">
+            <n-slider
+              v-model:value="value"
+              :marks="props.marks"
+              :step="step"
+              :max="props.max"
+              :min="props.min"
+              :tooltip="false"
+              :style="{ margin: '4px 0' }"
+            />
+          </div>
+          <div class="suffix">{{ value }} {{ usePercent ? '%' : '' }}</div>
+        </div>
+      </template>
+    </n-list-item>
+  </n-list>
+</template>
+
+<style scoped>
+.prefix {
+  display: flex;
+  flex-direction: row;
+  width: 20%;
+  min-width: 80px;
+  font-size: large;
+}
+
+.default {
+  display: flex;
+  flex: auto;
+  margin: 0;
+}
+
+.suffix {
+  display: flex;
+  flex-direction: row-reverse;
+  width: 15%;
+  min-width: 60px;
+  font-size: large;
+}
+</style>
