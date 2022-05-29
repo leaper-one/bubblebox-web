87ecef1ce522eca75aae742fb86214a83ccd3452
JulienZeng <77975074+JulienZeng@users.noreply.github.com>
2022-05-29 08:13:12 +0800

    右上角用户头像，昵称，登出组件实现

---
+++ src/components/TopBatUserCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useMessage } from 'naive-ui'
+
+const props = defineProps<{
+  avatarUrl: string
+  userName: string
+}>()
+
+const message = useMessage()
+
+const logOut = () => {
+  message.success('退出登录')
+}
+</script>
+
+<template>
+  <div class="flex align space-between">
+    <div style="min-width: 30px">
+      <n-avatar round :src="props.avatarUrl" />
+    </div>
+    <div style="min-width: 120px; font-weight: bold">{{ props.userName }}</div>
+    <div>
+      <n-button type="error" @click="logOut()">登出</n-button>
+    </div>
+  </div>
+</template>
