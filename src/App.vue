8f5f43f96ba1678f380f3ff85f4f27048aaf723b
JulienZeng <77975074+JulienZeng@users.noreply.github.com>
2022-05-29 08:22:48 +0800

    添加常用样式，添加调试所需页面

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,68 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import TopBar from './components/TopBar.vue'
+
+const pageTitle = 'OBS插件后台'
+const avatarUrl = 'https://photo.zjlblog.site/avatar.jpg'
+const userName = '测试名字'
 </script>
 
 <template>
-  <n-button> Hello NaiveUI </n-button>
+  <n-message-provider>
+    <TopBar :title="pageTitle" :avatar="avatarUrl" :userName="userName" />
+    <div id="main">
+      <router-view />
+    </div>
+  </n-message-provider>
 </template>
 
 <style>
 #app {
-  margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
   text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+#main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 30px;
+}
+
+.flex {
+  display: flex;
+}
+
+.align {
+  align-items: center;
+}
+
+.space-between {
+  justify-content: space-between;
+}
+
+.justify {
+  justify-content: center;
+}
+
+.row {
+  flex-direction: row;
+}
+
+.column {
+  flex-direction: column;
+}
+
+.row-reverse {
+  flex-direction: row-reverse;
+}
+
+.flex-auto {
+  flex: auto;
 }
 </style>
