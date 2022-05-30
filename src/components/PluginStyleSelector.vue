941ad05dbca51866845bf565b712987bcf4d1289
JulienZeng <77975074+JulienZeng@users.noreply.github.com>
2022-05-31 02:45:26 +0800

    添加样式选择组件

---
+++ src/components/PluginStyleSelector.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useMessage } from 'naive-ui'
+import { ref } from 'vue'
+const message = useMessage()
+const props = defineProps<{
+  styleSelect: string
+  styleOptions: Array<any>
+}>()
+
+const styleSelect = ref(props.styleSelect)
+
+const resetStyle = () => {
+  message.success('已成功重置')
+
+  // TODO：实现重置为默认样式
+}
+</script>
+
+<template>
+  <div>
+    <n-list bordered>
+      <n-list-item>
+        <template #suffix>
+          <n-button @click="resetStyle()">清空展示</n-button>
+        </template>
+        <n-select placeholder="请选择" v-model:value="styleSelect" :options="props.styleOptions" />
+      </n-list-item>
+    </n-list>
+  </div>
+</template>
