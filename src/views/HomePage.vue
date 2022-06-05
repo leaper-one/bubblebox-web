e16dea00dde45dfd4169cec77263577a796b0a0f
Aether <1252597401@qq.com>
2022-06-05 19:17:07 +0800

    主页

---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const appear = ref(true)
+</script>
+
+<template>
+  <div>
+    <div class="pt-30"></div>
+    <div class="text-8xl font-bold">
+      <n-collapse-transition :appear="appear">
+        <div class="text-left">Welcome</div>
+        <div class="p-3"></div>
+        <div class="text-blue-500">LEAPERone</div>
+      </n-collapse-transition>
+    </div>
+  </div>
+</template>
