54e870c704d14756e0fcaf1a1f494f64b9f20ee7
Aether <1252597401@qq.com>
2022-06-05 19:17:49 +0800

    登录页

---
+++ src/views/SignInPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="text-3xl font-bold text-blue-500 pb-3">LEAPERone</div>
+  <n-card class="max-w-28em">
+    <n-tabs
+      class="card-tabs text-left"
+      default-value="signin"
+      size="large"
+      animated
+      style="margin: 0 -4px"
+      pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
+    >
+      <n-tab-pane name="signin" tab="登录">
+        <n-form>
+          <n-form-item-row label="帐号">
+            <n-input />
+          </n-form-item-row>
+          <n-form-item-row label="密码">
+            <n-input />
+          </n-form-item-row>
+        </n-form>
+        <n-button type="primary" block secondary strong> 登录 </n-button>
+      </n-tab-pane>
+    </n-tabs>
+  </n-card>
+</template>
