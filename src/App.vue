301b06d627e48fe5af696bb264609d01985c5c51
JulienZeng <936636589@qq.com>
2022-07-11 15:20:57 +0800

    初步配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import TopBar from './components/TopBar.vue'
-
-const pageTitle = 'OBS插件后台'
-const avatarUrl = 'https://photo.zjlblog.site/avatar.jpg'
-const userName = '测试名字'
 </script>
 
 <template>
   <n-message-provider>
-    <TopBar :title="pageTitle" :avatar="avatarUrl" :userName="userName" />
-    <div class="flex flex-col items-center justify-center p-30px" id="main">
+    <div>
       <router-view />
     </div>
   </n-message-provider>
 </template>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,55 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'obs',
+    name: 'home',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ObsPage.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomePage.vue'),
+    meta: {
+      title: '泡泡树洞'
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/SignInPage.vue'),
+    meta: {
+      title: '泡泡树洞-登录'
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/SignUpPage.vue'),
+    meta: {
+      title: '泡泡树洞-注册'
+    }
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    redirect: '/admin/obs',
+    component: () => import('../Layout/LayoutPage.vue'),
+    children: [
+      {
+        path: '/admin/obs',
+        name: 'obs',
+        component: () => import('../views/ObsPage.vue'),
+        beforeEnter: () => {
+          // TODO: 获取登录信息
+          const isAuth = false
+          if (isAuth) {
+            return true
+          } else {
+            return { name: 'login' }
+          }
+        },
+        meta: {
+          title: '泡泡树洞-OBS插件管理'
+        }
+      }
+    ]
   }
 ]
 
